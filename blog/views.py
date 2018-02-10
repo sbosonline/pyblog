803adf89ccad6a69e7ee175363090874ad8def24
namdi <nnamsok@gmail.com>
2018-02-10 00:27:44 +0000

    view queryset review

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {'posts': Post.objects.all()})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)

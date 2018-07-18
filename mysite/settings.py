b34f05e5edb99f9571415ff618d144a58756ff24
namdi <nnamsok@gmail.com>
2018-07-18 14:19:58 +0100

    debug mode edit

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-yw*$&d(u3rc5f17)3tud8=9k_p4&s(@-p7fnmyxcy7pd29og1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','namdi.pythonanywhere.com']
 

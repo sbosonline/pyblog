9334b4ddcf811e2020800e37bd5280fa5578ef19
jasmaa <jasonmaa3955@gmail.com>
2018-10-01 00:01:53 -0400

    Fix comment adding and display bugs

--- blog/views.py
+++ blog/views.py
@@ -53,16 +53,17 @@ def post_publish(request,pk):
 
 def add_comment_to_post(request,pk):
     post=get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.post = post
-            comment.save()
-            return redirect('post_detail', pk=post.pk)
-        else:
-            form = CommentForm()
-        return render(request,'blog/add_comment_to_post.html',{'form':form})
+    
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+        return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+
+    return render(request,'blog/add_comment_to_post.html',{'form':form})
 
 @login_required
 def comment_approve(request,pk):
